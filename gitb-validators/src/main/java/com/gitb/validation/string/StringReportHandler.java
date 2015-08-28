c824c578082e71c319e29de87e83c1ecb61bff22
Şenan Postacı <senan@srdc.com.tr>
2015-08-28 15:12:21 +0300

    Added a simple StringValidator to compare two strings

---
+++ gitb-validators/src/main/java/com/gitb/validation/string/StringReportHandler.java
@@ -0,0 +1,55 @@
+package com.gitb.validation.string;
+
+import com.gitb.core.AnyContent;
+import com.gitb.core.ValueEmbeddingEnumeration;
+import com.gitb.tr.TAR;
+import com.gitb.tr.TestAssertionGroupReportsType;
+import com.gitb.tr.TestResultType;
+import com.gitb.types.BooleanType;
+import com.gitb.types.DataType;
+import com.gitb.types.StringType;
+import com.gitb.validation.common.AbstractReportHandler;
+
+/**
+ * Created by senan
+ */
+public class StringReportHandler extends AbstractReportHandler {
+    public static final String ACTUALSTRING_ITEM_NAME = "ACTUAL STRING";
+    public static final String EXPECTEDSTRING_ITEM_NAME = "EXPECTED STRING";
+
+    protected StringReportHandler(StringType actualstring, StringType expectedstring, BooleanType result) {
+        super();
+
+        report.setName("String Validation");
+        report.setReports(new TestAssertionGroupReportsType());
+
+        AnyContent attachment = new AnyContent();
+        attachment.setType(DataType.MAP_DATA_TYPE);
+
+        AnyContent xml = new AnyContent();
+        xml.setName(ACTUALSTRING_ITEM_NAME);
+        xml.setType(DataType.STRING_DATA_TYPE);
+        xml.setEmbeddingMethod(ValueEmbeddingEnumeration.STRING);
+        xml.setValue(new String(actualstring.serializeByDefaultEncoding()));
+        attachment.getItem().add(xml);
+
+        AnyContent schema = new AnyContent();
+        schema.setEmbeddingMethod(ValueEmbeddingEnumeration.STRING);
+        schema.setType(DataType.STRING_DATA_TYPE);
+        schema.setName(EXPECTEDSTRING_ITEM_NAME);
+        schema.setValue(new String(expectedstring.serializeByDefaultEncoding()));
+        attachment.getItem().add(schema);
+
+        if ((boolean) result.getValue()) {
+            report.setResult(TestResultType.SUCCESS);
+        } else {
+            report.setResult(TestResultType.FAILURE);
+        }
+        report.setContext(attachment);
+    }
+
+    @Override
+    public TAR createReport() {
+        return report;
+    }
+}
---
+++ gitb-validators/src/main/java/com/gitb/validation/string/StringValidator.java
@@ -0,0 +1,45 @@
+package com.gitb.validation.string;
+
+import com.gitb.core.Configuration;
+import com.gitb.tr.TestStepReportType;
+import com.gitb.types.BooleanType;
+import com.gitb.types.DataType;
+import com.gitb.types.StringType;
+import com.gitb.validation.IValidationHandler;
+import com.gitb.validation.common.AbstractValidator;
+import org.kohsuke.MetaInfServices;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by senan
+ */
+@MetaInfServices(IValidationHandler.class)
+public class StringValidator extends AbstractValidator{
+
+    private final String ACTUAL_STRING_ARGUMENT_NAME   = "actualstring";
+    private final String EXPECTED_STRING_ARGUMENT_NAME = "expectedstring";
+
+    private final String MODULE_DEFINITION_XML = "/string-validator-definition.xml";
+
+    public StringValidator() {
+        this.validatorDefinition = readModuleDefinition(MODULE_DEFINITION_XML);
+    }
+
+    @Override
+    public TestStepReportType validate(List<Configuration> configurations, Map<String, DataType> inputs) {
+        StringType actualString    = (StringType) inputs.get(ACTUAL_STRING_ARGUMENT_NAME);
+        StringType expectedString  = (StringType) inputs.get(EXPECTED_STRING_ARGUMENT_NAME);
+
+        String actualStringValue   = (String) actualString.getValue();
+        String expectedStringValue = (String) expectedString.getValue();
+
+        // process xpath
+        BooleanType result = new BooleanType(actualStringValue.equals(expectedStringValue));
+
+        // return report
+        StringReportHandler handler = new StringReportHandler(actualString, expectedString, result);
+        return handler.createReport();
+    }
+}
