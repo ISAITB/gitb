bd963bb7192bb7a7e7a76d68a3b7c441e3c7afc0
Costas Simatos <costas.simatos@sigmacubed.eu>
2018-09-28 15:05:02 +0200

    Cleanup and release preparation

--- gitb-messaging/src/main/java/com/gitb/messaging/layer/AbstractMessagingHandler.java
+++ gitb-messaging/src/main/java/com/gitb/messaging/layer/AbstractMessagingHandler.java
@@ -20,7 +20,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.net.UnknownHostException;
 import java.util.*;
 
 /**
@@ -258,13 +257,8 @@ public abstract class AbstractMessagingHandler implements IMessagingHandler {
 
     @Override
     public void endSession(String sessionId) {
-        try {
-            SessionManager sessionManager = SessionManager.getInstance();
-
-            sessionManager.endSession(sessionId);
-        } catch (UnknownHostException e) {
-            throw new GITBEngineInternalError(e);
-        }
+        SessionManager sessionManager = SessionManager.getInstance();
+        sessionManager.endSession(sessionId);
     }
 
     private void checkRequiredTypedParameters(List<TypedParameter> parameters, Message message) {
