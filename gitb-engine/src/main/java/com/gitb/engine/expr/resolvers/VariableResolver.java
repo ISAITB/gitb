4067871e93af64e0b17cbd0243ecd4afbbc1606f
Costas Simatos <costas.simatos@sigmacubed.eu>
2019-08-26 16:32:36 +0200

    [ITB-421] Corrected list index calculation for double numbers.

--- gitb-engine/src/main/java/com/gitb/engine/expr/resolvers/VariableResolver.java
+++ gitb-engine/src/main/java/com/gitb/engine/expr/resolvers/VariableResolver.java
@@ -262,7 +262,7 @@ public class VariableResolver implements XPathVariableResolver{
             throw new GITBEngineInternalError(ErrorUtils.errorInfo(ErrorCode.INVALID_TEST_CASE, "Invalid variable reference, you can use index or key only on container types"));
         }
         if(container instanceof ListType){
-            int index = Integer.parseInt(keyOrIndex);
+            int index = Double.valueOf(keyOrIndex).intValue();
             return ((ListType) container).getItem(index);
         }
         //MapType
