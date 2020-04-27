001ac7bd94d102e3dc17a69d01a38e06638c6191
Costas Simatos <costas.simatos@sigmacubed.eu>
2020-04-27 17:47:21 +0200

    [ITB-562] Comments on FlywayDB configuration.

--- gitb-ui/app/modules/Module.scala
+++ gitb-ui/app/modules/Module.scala
@@ -8,6 +8,10 @@ class Module extends play.api.inject.Module {
   def bindings(environment: play.api.Environment, configuration: play.api.Configuration) = {
     Seq(
       bind[BeforeStartHook].toSelf.eagerly,
+      /*
+       Calling here the initialisation of FlyWayDB (and not via its own module). The reason for this is to ensure a DB
+       is correctly created/migrated before we do other changes that may require DB interactions at start-up.
+       */
       bind[PlayInitializer].toSelf.eagerly,
       bind[PostStartHook].toSelf.eagerly,
       bind[OnStopHook].toSelf.eagerly
