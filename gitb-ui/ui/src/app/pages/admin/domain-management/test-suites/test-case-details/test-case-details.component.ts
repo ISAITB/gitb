205f6d4625f3bdf16a453a7fe5c57d969ccdfce4
Costas Simatos <costas.simatos@sigmacubed.eu>
2021-09-16 14:49:28 +0200

    [ITB-822] Test case diagram preview

--- gitb-ui/ui/src/app/pages/admin/domain-management/test-suites/test-case-details/test-case-details.component.ts
+++ gitb-ui/ui/src/app/pages/admin/domain-management/test-suites/test-case-details/test-case-details.component.ts
@@ -1,5 +1,9 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { mergeMap, Observable, share, map } from 'rxjs';
+import { ActorInfo } from 'src/app/components/diagram/actor-info';
+import { DiagramEvents } from 'src/app/components/diagram/diagram-events';
+import { StepData } from 'src/app/components/diagram/step-data';
 import { BaseComponent } from 'src/app/pages/base-component.component';
 import { ConformanceService } from 'src/app/services/conformance.service';
 import { DataService } from 'src/app/services/data.service';
@@ -7,13 +11,13 @@ import { HtmlService } from 'src/app/services/html.service';
 import { PopupService } from 'src/app/services/popup.service';
 import { RoutingService } from 'src/app/services/routing.service';
 import { TestSuiteService } from 'src/app/services/test-suite.service';
+import { TestService } from 'src/app/services/test.service';
 import { TestCase } from 'src/app/types/test-case';
 
 @Component({
   selector: 'app-test-case-details',
   templateUrl: './test-case-details.component.html',
-  styles: [
-  ]
+  styleUrls: [ './test-case-details.component.less' ]
 })
 export class TestCaseDetailsComponent extends BaseComponent implements OnInit, AfterViewInit {
 
@@ -24,6 +28,10 @@ export class TestCaseDetailsComponent extends BaseComponent implements OnInit, A
   testCaseId!:number
   showDocumentation = false
   pending = false
+  diagramLoaded = false
+  steps: {[key: string]: StepData[]} = {}
+  actorInfo: {[key: string]: ActorInfo[]} = {}
+  testEvents: {[key: number]: DiagramEvents} = {}
 
   constructor(
     private dataService: DataService,
@@ -32,7 +40,8 @@ export class TestCaseDetailsComponent extends BaseComponent implements OnInit, A
     private route: ActivatedRoute,
     private popupService: PopupService,
     private htmlService: HtmlService,
-    private conformanceService: ConformanceService
+    private conformanceService: ConformanceService,
+    private testService: TestService
   ) { super() }
 
   ngAfterViewInit(): void {
@@ -47,6 +56,10 @@ export class TestCaseDetailsComponent extends BaseComponent implements OnInit, A
 		this.testSuiteService.getTestCase(this.testCaseId)
     .subscribe((data) => {
 			this.testCase = data
+    })
+    this.testEvents[this.testCaseId] = new DiagramEvents()    
+    this.getTestCaseDefinition(this.testCaseId).subscribe(() => {}).add(() => {
+      this.diagramLoaded = true
     })
   }
 
@@ -73,6 +86,40 @@ export class TestCaseDetailsComponent extends BaseComponent implements OnInit, A
 
 	saveDisabled() {
 		return !this.textProvided(this.testCase?.sname)
+  }
+
+  getTestCaseDefinition(testCaseToLookup: number): Observable<void> {
+    return this.testService.getTestCaseDefinition(testCaseToLookup).pipe(
+      mergeMap((testCase) => {
+        return this.testService.getActorDefinitions(this.specificationId).pipe(
+          map((data) => {
+            let tempActors = testCase.actors.actor
+            for (let domainActorData of data) {
+              for (let testCaseActorData of tempActors) {
+                if (testCaseActorData.id == domainActorData.actorId) {
+                  if (testCaseActorData.name == undefined) {
+                    testCaseActorData.name = domainActorData.name
+                  }
+                  if (testCaseActorData.displayOrder == undefined && domainActorData.displayOrder != undefined) {
+                    testCaseActorData.displayOrder = domainActorData.displayOrder
+                  }
+                  break
+                }
+              }
+            }
+            tempActors = tempActors.sort((a, b) => {
+              if (a.displayOrder == undefined && b.displayOrder == undefined) return 0
+              else if (a.displayOrder != undefined && b.displayOrder == undefined) return -1
+              else if (a.displayOrder == undefined && b.displayOrder != undefined) return 1
+              else return Number(a.displayOrder) - Number(b.displayOrder)
+            })
+            this.actorInfo[testCaseToLookup] = tempActors as ActorInfo[]
+            this.steps[testCaseToLookup] = testCase.steps
+            this.testEvents[testCaseToLookup].signalTestLoad({ testId: testCaseToLookup })
+          })
+        )
+      }), share()
+    )
   }
 
 }
