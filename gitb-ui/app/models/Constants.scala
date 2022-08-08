7227189a8ce1031e9cb59d65ae976b53f741b94f
simatosc <costas.simatos@sigmacubed.eu>
2022-08-08 09:18:45 +0200

    Bumped to snapshot version.

--- gitb-ui/app/models/Constants.scala
+++ gitb-ui/app/models/Constants.scala
@@ -50,6 +50,6 @@ object Constants {
   val FilterDateFormat = "dd-MM-yyyy HH:mm:ss"
   val AutomationHeader = "ITB_API_KEY"
 
-  val VersionNumber = "1.17.0"
-  val ResourceVersionNumber = "1.17.0"
+  val VersionNumber = "1.18.0-snapshot"
+  val ResourceVersionNumber = "1.18.0-snapshot"
 }
