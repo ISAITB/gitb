0f9b61bbdea6747a243890241485df02c75c084a
simatosc <costas.simatos@sigmacubed.eu>
2023-09-06 11:17:17 +0200

    [ITB-1485] Return a 404 "Not found" response for any request to a non-existent path

--- gitb-ui/app/filters/AuthenticationFilter.scala
+++ gitb-ui/app/filters/AuthenticationFilter.scala
@@ -4,21 +4,21 @@ import akka.stream.Materializer
 import com.gitb.utils.HmacUtils
 import config.Configurations
 import config.Configurations.API_ROOT
+import controllers.util.ResponseConstructor.NotFound
 import controllers.util.{Parameters, ResponseConstructor}
 import exceptions._
-
-import javax.inject.Inject
-import org.slf4j.{Logger, LoggerFactory}
-import managers.AccountManager
 import models.Constants
+import org.slf4j.{Logger, LoggerFactory}
 import persistence.cache.TokenCache
 import play.api.mvc.Results.Unauthorized
 import play.api.mvc._
+import play.api.routing.Router
 import play.mvc.Http.HeaderNames._
 
+import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
-class AuthenticationFilter @Inject() (implicit ec: ExecutionContext, implicit val mat: Materializer, accountManager: AccountManager) extends Filter {
+class AuthenticationFilter @Inject() (implicit ec: ExecutionContext, implicit val mat: Materializer, router: Router) extends Filter {
 
   private final val logger: Logger = LoggerFactory.getLogger(classOf[AuthenticationFilter])
 
@@ -35,82 +35,90 @@ class AuthenticationFilter @Inject() (implicit ec: ExecutionContext, implicit va
 
   def apply(next: (RequestHeader) => Future[Result])
            (requestHeader: RequestHeader): Future[Result] = {
-    //if public service called, execute it immediately
-    if (isPublic(requestHeader)){
-      next(requestHeader)
+    if (router.handlerFor(requestHeader).isEmpty) {
+      // If this is an unknown route stop any further processing and return a 404.
+      Future {
+        logger.debug("Received request for non-existent path [{}]", requestHeader.path)
+        NotFound("")
+      }
     } else {
-      // Check Authorization headers
-      val authzHeader = requestHeader.headers.get(AUTHORIZATION)
-      if (authzHeader.isDefined) {
-        try {
-          // Parse access token info
-          val list = authzHeader.get.split(BEARER + " ")
-          if (list.length == 2){
-            next(downstreamHeaderFromAccessToken(list(1), requestHeader))
-          } else{
-            //There is a problem with the authorization header
-            Future{
-              ResponseConstructor.constructUnauthorizedResponse(ErrorCodes.INVALID_AUTHORIZATION_HEADER, "Invalid authorization header")
-            }
-          }
-        } catch {
-          // Catch-all for authorization header problems
-          case e: Exception => {
-            Future{
-              ResponseConstructor.constructUnauthorizedResponse(ErrorCodes.INVALID_AUTHORIZATION_HEADER, "Invalid authorization header")
-            }
-          }
-        }
+      //if public service called, execute it immediately
+      if (isPublic(requestHeader)){
+        next(requestHeader)
       } else {
-        if (isAuthenticatedHttpAccessAllowed(requestHeader)) {
-          val sessionCookie = requestHeader.cookies.get("tat")
-          if (sessionCookie.isDefined) {
-            next(downstreamHeaderFromAccessToken(sessionCookie.get.value, requestHeader))
-          } else {
-            Future { Unauthorized }
-          }
-        } else if (isPublicWithOptionalAuthentication(requestHeader)) {
-          // No authentication token but not a problem.
-          next(requestHeader)
-        } else {
-          if (isHmacAuthenticationAllowed(requestHeader)) {
-            val hmacHeader = requestHeader.headers.get(HmacUtils.HMAC_HEADER_TOKEN)
-            if (hmacHeader.isDefined) {
-              next(requestHeader)
-            } else {
-              //Requires authorization to execute this service
-              logger.warn("Request blocked due to missing user or HMAC authentication token [" + requestHeader.path + "]")
-              Future {
-                ResponseConstructor.constructUnauthorizedResponse(ErrorCodes.AUTHORIZATION_REQUIRED, "Needs authorization header")
+        // Check Authorization headers
+        val authzHeader = requestHeader.headers.get(AUTHORIZATION)
+        if (authzHeader.isDefined) {
+          try {
+            // Parse access token info
+            val list = authzHeader.get.split(BEARER + " ")
+            if (list.length == 2){
+              next(downstreamHeaderFromAccessToken(list(1), requestHeader))
+            } else{
+              //There is a problem with the authorization header
+              Future{
+                ResponseConstructor.constructUnauthorizedResponse(ErrorCodes.INVALID_AUTHORIZATION_HEADER, "Invalid authorization header")
               }
             }
-          } else if (isAutomationAccessAllowed(requestHeader)) {
-            if (Configurations.AUTOMATION_API_ENABLED) {
-              if (isPublicAutomationAccessAllowed(requestHeader)) {
+          } catch {
+            // Catch-all for authorization header problems
+            case e: Exception => {
+              Future{
+                ResponseConstructor.constructUnauthorizedResponse(ErrorCodes.INVALID_AUTHORIZATION_HEADER, "Invalid authorization header")
+              }
+            }
+          }
+        } else {
+          if (isAuthenticatedHttpAccessAllowed(requestHeader)) {
+            val sessionCookie = requestHeader.cookies.get("tat")
+            if (sessionCookie.isDefined) {
+              next(downstreamHeaderFromAccessToken(sessionCookie.get.value, requestHeader))
+            } else {
+              Future { Unauthorized }
+            }
+          } else if (isPublicWithOptionalAuthentication(requestHeader)) {
+            // No authentication token but not a problem.
+            next(requestHeader)
+          } else {
+            if (isHmacAuthenticationAllowed(requestHeader)) {
+              val hmacHeader = requestHeader.headers.get(HmacUtils.HMAC_HEADER_TOKEN)
+              if (hmacHeader.isDefined) {
                 next(requestHeader)
               } else {
-                val apiKeyHeader = requestHeader.headers.get(Constants.AutomationHeader)
-                if (apiKeyHeader.isDefined) {
+                //Requires authorization to execute this service
+                logger.warn("Request blocked due to missing user or HMAC authentication token [" + requestHeader.path + "]")
+                Future {
+                  ResponseConstructor.constructUnauthorizedResponse(ErrorCodes.AUTHORIZATION_REQUIRED, "Needs authorization header")
+                }
+              }
+            } else if (isAutomationAccessAllowed(requestHeader)) {
+              if (Configurations.AUTOMATION_API_ENABLED) {
+                if (isPublicAutomationAccessAllowed(requestHeader)) {
                   next(requestHeader)
                 } else {
-                  //Requires authorization to execute this service
-                  logger.warn("Request blocked due to missing API key header [" + requestHeader.path + "]")
-                  Future {
-                    ResponseConstructor.constructUnauthorizedResponse(ErrorCodes.AUTHORIZATION_REQUIRED, "Needs API key header")
+                  val apiKeyHeader = requestHeader.headers.get(Constants.AutomationHeader)
+                  if (apiKeyHeader.isDefined) {
+                    next(requestHeader)
+                  } else {
+                    //Requires authorization to execute this service
+                    logger.warn("Request blocked due to missing API key header [" + requestHeader.path + "]")
+                    Future {
+                      ResponseConstructor.constructUnauthorizedResponse(ErrorCodes.AUTHORIZATION_REQUIRED, "Needs API key header")
+                    }
                   }
+                }
+              } else {
+                logger.warn("Request blocked because the Test Bed's automation API is disabled [" + requestHeader.path + "]")
+                Future {
+                  ResponseConstructor.constructUnauthorizedResponse(ErrorCodes.UNAUTHORIZED_ACCESS, "Automation API not enabled")
                 }
               }
             } else {
-              logger.warn("Request blocked because the Test Bed's automation API is disabled [" + requestHeader.path + "]")
-              Future {
-                ResponseConstructor.constructUnauthorizedResponse(ErrorCodes.UNAUTHORIZED_ACCESS, "Automation API not enabled")
+              //Requires authorization to execute this service
+              logger.warn("Request blocked due to missing user token ["+requestHeader.path+"]")
+              Future{
+                ResponseConstructor.constructUnauthorizedResponse(ErrorCodes.AUTHORIZATION_REQUIRED, "Needs authorization header")
               }
-            }
-          } else {
-            //Requires authorization to execute this service
-            logger.warn("Request blocked due to missing user token ["+requestHeader.path+"]")
-            Future{
-              ResponseConstructor.constructUnauthorizedResponse(ErrorCodes.AUTHORIZATION_REQUIRED, "Needs authorization header")
             }
           }
         }
