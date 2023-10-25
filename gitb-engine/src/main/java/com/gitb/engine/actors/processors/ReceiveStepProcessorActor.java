b0c08cc9c97a005aeda79bdf8b5a45b402af5fac
simatosc <costas.simatos@sigmacubed.eu>
2023-10-25 13:44:41 +0200

    Signal a test step timeout (set as a failure) as an error in the log.

--- gitb-engine/src/main/java/com/gitb/engine/actors/processors/ReceiveStepProcessorActor.java
+++ gitb-engine/src/main/java/com/gitb/engine/actors/processors/ReceiveStepProcessorActor.java
@@ -176,7 +176,6 @@ public class ReceiveStepProcessorActor extends AbstractMessagingStepProcessorAct
 				signalStepStatus(handleMessagingResult(((NotificationReceived) message).getReport()));
 			} else if (message instanceof TimeoutExpired) {
 				if (!receivedResponse) {
-					logger.debug(addMarker(), "Timeout expired while waiting to receive message");
 					VariableResolver resolver = new VariableResolver(scope);
 					String flagName = null;
 					if (!StringUtils.isBlank(step.getTimeoutFlag())) {
@@ -193,6 +192,11 @@ public class ReceiveStepProcessorActor extends AbstractMessagingStepProcessorAct
 						} else {
 							errorIfTimeout = Boolean.parseBoolean(step.getTimeoutIsError());
 						}
+					}
+					if (errorIfTimeout) {
+						logger.error(addMarker(), "Timeout expired while waiting to receive message");
+					} else {
+						logger.debug(addMarker(), "Timeout expired while waiting to receive message");
 					}
 					signalStepStatus(handleMessagingResult(MessagingHandlerUtils.getMessagingReportForTimeout(flagName, errorIfTimeout)));
 				}
