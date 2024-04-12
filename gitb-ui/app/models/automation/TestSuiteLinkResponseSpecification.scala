5011a3b4f3adeb6f786c6e0eed12587453517d29
simatosc <costas.simatos@sigmacubed.eu>
2024-04-12 17:40:03 +0200

    [ITB-1588] Return test suite metadata following a test suite deployment via REST API

--- gitb-ui/app/models/automation/TestSuiteLinkResponseSpecification.scala
+++ gitb-ui/app/models/automation/TestSuiteLinkResponseSpecification.scala
@@ -1,7 +1,15 @@
 package models.automation
 
 case class TestSuiteLinkResponseSpecification(
-  specification: String,
+  specificationKey: String,
+  specificationId: Option[Long],
   linked: Boolean,
-  message: Option[String]
-)
+  message: Option[String],
+  actors: Option[List[KeyValueRequired]] = None
+) {
+
+  def withActorIdentifiers(actorInfo: List[KeyValueRequired]): TestSuiteLinkResponseSpecification = {
+    TestSuiteLinkResponseSpecification(specificationKey, specificationId, linked, message, Some(actorInfo))
+  }
+
+}
