95700faba18ffbf688520ade7118a43ca9c85c58
simatosc <costas.simatos@sigmacubed.eu>
2024-06-06 10:04:17 +0200

    [ITB-1618] Remove the obsolete X-XSS-Protection header

--- gitb-ui/app/filters/HeaderFilter.scala
+++ gitb-ui/app/filters/HeaderFilter.scala
@@ -15,7 +15,6 @@ class HeaderFilter @Inject() (implicit val mat: Materializer, ec: ExecutionConte
         "X-Content-Type-Options" -> "nosniff",
         "Referrer-Policy" -> "same-origin",
         "X-Frame-Options" -> "deny",
-        "X-Xss-Protection" -> "X-XSS-Protection: 1; mode=block",
         "Feature-Policy" -> "accelerometer 'none'; autoplay 'none'; camera 'none'; encrypted-media 'none'; fullscreen 'none'; geolocation 'none'; gyroscope 'none'; magnetometer 'none'; microphone 'none'; midi 'none'; payment 'none'; picture-in-picture 'none'; sync-xhr 'self'; xr-spatial-tracking 'none';",
         "Content-Security-Policy" -> "default-src 'none'; font-src 'self'; script-src 'self'; connect-src 'self'; style-src 'unsafe-inline' 'self'; img-src * data:; base-uri 'self'; frame-ancestors 'none';"
       )
