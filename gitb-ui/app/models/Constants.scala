d504f87f4c81100b4fd0b8ee61cde71c656d135f
simatosc <costas.simatos@sigmacubed.eu>
2024-06-28 13:41:31 +0200

    Preparation for release 1.23.1

--- gitb-ui/app/models/Constants.scala
+++ gitb-ui/app/models/Constants.scala
@@ -62,6 +62,6 @@ object Constants {
 
   // When ending in "-snapshot", this is considered a non-published release.
   val VersionNumber = "1.24.0-snapshot"
-  val VersionNumberPostfixForResources = ""
+  val VersionNumberPostfixForResources = "_1"
 
 }
