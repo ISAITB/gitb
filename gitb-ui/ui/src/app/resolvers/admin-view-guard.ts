23bca3ab464a125c3dba3af697311007e3073ab8
simatosc <costas.simatos@sigmacubed.eu>
2024-10-10 16:09:09 +0200

    Load user information if needed in route guards

--- gitb-ui/ui/src/app/resolvers/admin-view-guard.ts
+++ gitb-ui/ui/src/app/resolvers/admin-view-guard.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { map, mergeMap, Observable, of, share } from 'rxjs';
-import { AuthProviderService } from '../services/auth-provider.service';
+import { mergeMap, Observable, of } from 'rxjs';
 import { DataService } from '../services/data.service';
 import { RoutingService } from '../services/routing.service';
 import { ProfileResolver } from './profile-resolver';
@@ -15,21 +14,34 @@ export class AdminViewGuard  {
   constructor(
     private dataService: DataService,
     private errorService: ErrorService,
-    private routingService: RoutingService
+    private routingService: RoutingService,
+    private profileResolver: ProfileResolver
   ) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      if (this.dataService.isCommunityAdmin || this.dataService.isSystemAdmin) {
-        return true
-      } else {
-        return this.errorService.showUnauthorisedAccessError().pipe(
-          mergeMap(() => {
-            return this.routingService.toHome()
-          })
-        )
-      }
+      return this.userLoaded().pipe(
+        mergeMap(() => {
+          if (this.dataService.isCommunityAdmin || this.dataService.isSystemAdmin) {
+            return of(true)
+          } else {
+            return this.errorService.showUnauthorisedAccessError().pipe(
+              mergeMap(() => {
+                return this.routingService.toHome()
+              })
+            )
+          }
+        })
+      )
+  }
+
+  private userLoaded(): Observable<any> {
+    if (this.dataService.user) {
+      return of(true)
+    } else {
+      return this.profileResolver.resolveData()
+    }
   }
 
 }
--- gitb-ui/ui/src/app/resolvers/system-admin-view-guard.ts
+++ gitb-ui/ui/src/app/resolvers/system-admin-view-guard.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { mergeMap, Observable } from 'rxjs';
+import { mergeMap, Observable, of } from 'rxjs';
 import { DataService } from '../services/data.service';
 import { RoutingService } from '../services/routing.service';
 import { ErrorService } from '../services/error.service';
+import { ProfileResolver } from './profile-resolver';
 
 @Injectable({
   providedIn: 'root'
@@ -13,21 +14,34 @@ export class SystemAdminViewGuard  {
   constructor(
     private dataService: DataService,
     private errorService: ErrorService,
-    private routingService: RoutingService
+    private routingService: RoutingService,
+    private profileResolver: ProfileResolver
   ) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      if (this.dataService.isSystemAdmin) {
-        return true
-      } else {
-        return this.errorService.showUnauthorisedAccessError().pipe(
-          mergeMap(() => {
-            return this.routingService.toHome()
-          })
-        )
-      }
+      return this.userLoaded().pipe(
+        mergeMap(() => {
+          if (this.dataService.isCommunityAdmin || this.dataService.isSystemAdmin) {
+            return of(true)
+          } else {
+            return this.errorService.showUnauthorisedAccessError().pipe(
+              mergeMap(() => {
+                return this.routingService.toHome()
+              })
+            )
+          }
+        })
+      )
+  }
+
+  private userLoaded(): Observable<any> {
+    if (this.dataService.user) {
+      return of(true)
+    } else {
+      return this.profileResolver.resolveData()
+    }
   }
 
 }
