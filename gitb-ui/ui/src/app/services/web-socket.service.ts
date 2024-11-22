f960eeabadd70c805758853a257e48d48f9b998e
simatosc <costas.simatos@sigmacubed.eu>
2024-11-22 16:56:02 +0100

    [ITB-1716] Support a configurable web application context for the gitb-ui application

--- gitb-ui/ui/src/app/services/web-socket.service.ts
+++ gitb-ui/ui/src/app/services/web-socket.service.ts
@@ -3,17 +3,18 @@ import { NextObserver } from 'rxjs';
 import { webSocket } from 'rxjs/webSocket';
 import { ROUTES } from '../common/global';
 import { Utils } from '../common/utils';
+import {DataService} from './data.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebSocketService {
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   connect(openObserver: NextObserver<Event>, closeObserver: NextObserver<CloseEvent>) {
     return webSocket({
-      url: Utils.webSocketURL(ROUTES.controllers.WebSocketService.socket("session").webSocketURL()),
+      url: Utils.webSocketURL(this.dataService.completePath(ROUTES.controllers.WebSocketService.socket("session").webSocketURL())),
       openObserver: openObserver,
       closeObserver: closeObserver
     })
