eef5206aec362b4c8c6333bece8ca02efbc6690f
simatosc <costas.simatos@sigmacubed.eu>
2024-12-11 19:25:49 +0100

    [ITB-1356] Allow group steps to apply stopOnError semantics only for their child steps

--- gitb-engine/src/main/java/com/gitb/engine/actors/processors/SequenceProcessorActor.java
+++ gitb-engine/src/main/java/com/gitb/engine/actors/processors/SequenceProcessorActor.java
@@ -158,9 +158,9 @@ public class SequenceProcessorActor<T extends Sequence> extends AbstractTestStep
             if (scope.getContext().getCurrentState() != TestCaseContext.TestCaseStateEnum.STOPPING && scope.getContext().getCurrentState() != TestCaseContext.TestCaseStateEnum.STOPPED) {
                 if (status == StepStatus.ERROR && childStep instanceof TestConstruct construct) {
                     boolean childStopOnError = Boolean.TRUE.equals(construct.isStopOnError());
+                    boolean currentStopOnError = Boolean.TRUE.equals(step.isStopOnError());
                     if (childStopOnError) {
-                        boolean parentStopOnError = Boolean.TRUE.equals(step.isStopOnError());
-                        if (parentStopOnError) {
+                        if (currentStopOnError && !Boolean.FALSE.equals(step.isStopOnChildError()) || !currentStopOnError && step.isStopOnChildError() == null) {
                             // The child step was set to stopOnError. Stop processing and signal stop.
                             // We only signal a stop if the parent is also set to stop on error. This can come up if stopOnChildError was set to false on the parent.
                             scope.getContext().setCurrentState(TestCaseContext.TestCaseStateEnum.STOPPING);
@@ -169,10 +169,17 @@ public class SequenceProcessorActor<T extends Sequence> extends AbstractTestStep
                             } catch (Exception e) {
                                 LOG.error(addMarker(), "Error sending the signal to stop the test session from test step actor [{}].", stepId);
                             }
+                        } else {
+                            if (!Boolean.TRUE.equals(step.isStopOnChildError())) {
+                                // We ignore child errors - proceed.
+                                nextStep = startTestStepAtIndex(completedStepIndex + 1);
+                            }
                         }
                     } else {
-                        // We ignore child errors - proceed.
-                        nextStep = startTestStepAtIndex(completedStepIndex + 1);
+                        if (!Boolean.TRUE.equals(step.isStopOnChildError())) {
+                            // We ignore child errors - proceed.
+                            nextStep = startTestStepAtIndex(completedStepIndex + 1);
+                        }
                     }
                 } else {
                     // Proceed.
