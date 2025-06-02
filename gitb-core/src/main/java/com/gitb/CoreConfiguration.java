9efea5c0af474535e243aafe683a27d715d9c3e1
simatosc <costas.simatos@sigmacubed.eu>
2025-06-02 11:06:44 +0200

    Config file lookup correction

--- gitb-core/src/main/java/com/gitb/CoreConfiguration.java
+++ gitb-core/src/main/java/com/gitb/CoreConfiguration.java
@@ -2,8 +2,11 @@ package com.gitb;
 
 import org.apache.commons.configuration2.CompositeConfiguration;
 import org.apache.commons.configuration2.SystemConfiguration;
-import org.apache.commons.configuration2.builder.fluent.Configurations;
 import org.apache.commons.configuration2.ex.ConfigurationException;
+
+import java.io.IOException;
+
+import static com.gitb.utils.ConfigUtils.getPropertiesConfiguration;
 
 /**
  * Created by serbay on 9/8/14.
@@ -13,18 +16,17 @@ public class CoreConfiguration {
 
 	public static String TEST_CASE_REPOSITORY;
 
-    /**
+    /*
      * Load the configurations from the configuration files
      */
 	static {
 		try {
 			CompositeConfiguration config = new CompositeConfiguration();
 			config.addConfiguration(new SystemConfiguration());
-			config.addConfiguration(new Configurations().properties("core-module.properties"));
-
+			config.addConfiguration(getPropertiesConfiguration("core-module.properties"));
 			TEST_CASE_REPOSITORY = config.getString("gitb.test-case-repository");
 			// TODO load configuration parameters
-		} catch (ConfigurationException e) {
+		} catch (ConfigurationException | IOException e) {
 			throw new IllegalStateException("Error loading configuration", e);
 		}
 
