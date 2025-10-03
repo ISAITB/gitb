fdd999adf3e199cceeb663304744a6216bbcaeb9
simatosc <costas.simatos@sigmacubed.eu>
2025-10-03 11:21:30 +0200

    Typo correction

--- gitb-ui/conf/db/migration/default/V124__New_community_permissions_and_self_registration_settings.sql
+++ gitb-ui/conf/db/migration/default/V124__New_community_permissions_and_self_registration_settings.sql
@@ -1,4 +1,4 @@
-.ALTER TABLE `communities` ADD COLUMN `allow_user_management` TINYINT DEFAULT 1 NOT NULL;
+ALTER TABLE `communities` ADD COLUMN `allow_user_management` TINYINT DEFAULT 1 NOT NULL;
 ALTER TABLE `communities` ADD COLUMN `selfreg_allow_org_tokens` TINYINT DEFAULT 0 NOT NULL;
 ALTER TABLE `communities` ADD COLUMN `selfreg_allow_org_token_management` TINYINT DEFAULT 0 NOT NULL;
 ALTER TABLE `communities` ADD COLUMN `selfreg_force_org_token_input` TINYINT DEFAULT 0 NOT NULL;
