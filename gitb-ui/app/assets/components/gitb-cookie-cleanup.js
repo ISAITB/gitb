ee533bdfc3a20bb6b293eb132119e53cb77d58a5
simatosc <costas.simatos@sigmacubed.eu>
2026-01-20 16:29:44 +0100

    [ITB-2033] Accessing the welcome page invalidates the user's current session

--- gitb-ui/app/assets/components/gitb-cookie-cleanup.js
+++ gitb-ui/app/assets/components/gitb-cookie-cleanup.js
@@ -37,8 +37,6 @@ function eraseCookies() {
     let cookiePath = document.getElementById('cp-div').textContent;
     document.cookie = 'LOGIN_OPTION=;Path=/;SameSite=Strict;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
     document.cookie = 'ITB_REQUESTED_URL=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    document.cookie = 'tat=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    document.cookie = 'tat=;path='+cookiePath+';expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
 $(document).ready(function() {
     eraseCookies();
