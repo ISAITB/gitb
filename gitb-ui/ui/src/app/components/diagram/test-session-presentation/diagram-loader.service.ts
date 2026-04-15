3d50cc2587aff20695a153f1d6130de1f281bb79
simatosc <costas.simatos@sigmacubed.eu>
2026-04-15 17:39:23 +0200

    [ITB-2100] Pending iteration steps displayed in the test session history show as skipped

--- gitb-ui/ui/src/app/components/diagram/test-session-presentation/diagram-loader.service.ts
+++ gitb-ui/ui/src/app/components/diagram/test-session-presentation/diagram-loader.service.ts
@@ -227,8 +227,8 @@ export class DiagramLoaderService {
             }
           }
         } else if (step.type == "loop") {
-          // By default set the steps of the loop as skipped. Normal status values for executed iterations will be looked up via the sequences.
-          this.addStatusToSteps(sessionId, step.steps, testResultFlat, Constants.TEST_STATUS.SKIPPED)
+          // By default, set the steps of the loop as waiting. Normal status values for executed iterations will be looked up via the sequences.
+          this.addStatusToSteps(sessionId, step.steps, testResultFlat, Constants.TEST_STATUS.WAITING)
           this.traverseLoops(testResultFlat, step)
           if (step.sequences != undefined) {
             for (let sequence of step.sequences) {
