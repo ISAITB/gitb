abde0a532019a81ecac773f766e0c7b82f212764
simatosc <costas.simatos@sigmacubed.eu>
2026-06-03 12:17:59 +0200

    Show no-report message in non-TAR step reports

--- gitb-ui/ui/src/app/components/diagram/report/test-step-report-sr/test-step-report-sr.component.ts
+++ gitb-ui/ui/src/app/components/diagram/report/test-step-report-sr/test-step-report-sr.component.ts
@@ -17,7 +17,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
     selector: '[app-test-step-report-sr]',
-    template: '<div class="step-report simple-step-report"></div>',
+    templateUrl: './test-step-report-sr.component.html',
     standalone: false
 })
 export class TestStepReportSRComponent {
